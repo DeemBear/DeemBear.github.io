c5f03d98c27c8f2b8da8c163759c9a286b3e5c23
DeemBear <github.io@gmail.com>
2023-05-11 14:14:30 +0800

    test

--- resources/web/js/module/aToos/nBGGFetch.js
+++ resources/web/js/module/aToos/nBGGFetch.js
@@ -1,13 +1,19 @@
 /**
  * 自定义的fetch，用于返回自定义异常
+ * @param url {String} 请求的url
+ * @param rr {Object,undefined} 要添加的请求头
+ * @param noAddHeader {boolean,undefined} 是否不添加用于标示的请求头
+ *
  * */
-export default async function nBGGFetch(url,rr){
-    if(!rr){
-        rr = {headers:{"DeemBear":"true"}};
-    }else if(!rr.headers){
-        rr.headers = {"DeemBear":"true"};
-    }else {
-        rr.headers['DeemBear'] = "true";
+export default async function nBGGFetch(url,rr,noAddHeader){
+    if(!noAddHeader){
+        if(!rr){
+            rr = {headers:{"DeemBear":"true"}};
+        }else if(!rr.headers){
+            rr.headers = {"DeemBear":"true"};
+        }else {
+            rr.headers['DeemBear'] = "true";
+        }
     }
    let re = await fetch(url,rr)
    if(re.headers.get('DeemBearError')){
