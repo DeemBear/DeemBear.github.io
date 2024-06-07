40514474b407640c6ec82b8304b48f65935a1274
DeemBear <github.io@gmail.com>
2024-06-07 16:19:28 +0800

    test

--- resources/note-static/js/app.js
+++ resources/note-static/js/app.js
@@ -21,7 +21,7 @@ const passwdPrompt = () => {
     if (!passwd.trim()) {
         alert('密码不能为空!')
     }
-    const path = location.pathname
+    const path = encodeURIComponent(location.pathname); // 对路径进行编码
     window.fetch(`${path}/auth`, {
         method: 'POST',
         headers: {
@@ -106,7 +106,7 @@ window.addEventListener('DOMContentLoaded', function () {
             const passwd = window.prompt('输入新密码（留空可以清除当前密码）')
             if (passwd == null) return;
 
-            const path = window.location.pathname
+            const path = encodeURIComponent(window.location.pathname); // 对路径进行编码
             window.fetch(`${path}/pw`, {
                 method: 'POST',
                 headers: {
@@ -130,7 +130,7 @@ window.addEventListener('DOMContentLoaded', function () {
     if ($modeBtn) {
         $modeBtn.onclick = function (e) {
             const isMd = e.target.checked
-            const path = window.location.pathname
+            const path = encodeURIComponent(window.location.pathname); // 对路径进行编码
             window.fetch(`${path}/setting`, {
                 method: 'POST',
                 headers: {
@@ -155,7 +155,7 @@ window.addEventListener('DOMContentLoaded', function () {
     if ($shareBtn) {
         $shareBtn.onclick = function (e) {
             const isShare = e.target.checked
-            const path = window.location.pathname
+            const path = encodeURIComponent(window.location.pathname); // 对路径进行编码
             window.fetch(`${path}/setting`, {
                 method: 'POST',
                 headers: {
