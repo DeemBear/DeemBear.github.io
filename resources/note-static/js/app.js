d9a47ff17d83ae9dfdc7ee30f5de4dbc6a92c848
DeemBear <github.io@gmail.com>
2024-06-07 15:21:08 +0800

    test

---
+++ resources/note-static/js/app.js
@@ -0,0 +1,205 @@
+const errHandle = (err) => {
+    alert(`出错了: ${err}`)
+}
+const throttle = (func, delay) => {
+    let tid = null
+
+    return (...arg) => {
+        if (tid) return;
+
+        tid = setTimeout(() => {
+            func(...arg)
+            tid = null
+        }, delay)
+    }
+}
+
+const passwdPrompt = () => {
+    const passwd = window.prompt('请输入密码')
+    if (passwd == null) return;
+
+    if (!passwd.trim()) {
+        alert('密码不能为空!')
+    }
+    const path = location.pathname
+    window.fetch(`${path}/auth`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            passwd,
+        }),
+    })
+        .then(res => res.json())
+        .then(res => {
+            if (res.err !== 0) {
+                return errHandle(res.msg)
+            }
+            if (res.data.refresh) {
+                window.location.reload()
+            }
+        })
+        .catch(err => errHandle(err))
+}
+
+const renderPlain = (node, text) => {
+    if (node) {
+        node.innerHTML = DOMPurify.sanitize(text)
+    }
+}
+
+const renderMarkdown = (node, text) => {
+    if (node) {
+        const parseText = marked.parse(text)
+        node.innerHTML = DOMPurify.sanitize(parseText)
+    }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    const $textarea = document.querySelector('#contents')
+    const $loading = document.querySelector('#loading')
+    const $pwBtn = document.querySelector('.opt-pw')
+    const $modeBtn = document.querySelector('.opt-mode > input')
+    const $shareBtn = document.querySelector('.opt-share > input')
+    const $previewPlain = document.querySelector('#preview-plain')
+    const $previewMd = document.querySelector('#preview-md')
+    const $shareModal = document.querySelector('.share-modal')
+    const $closeBtn = document.querySelector('.share-modal .close-btn')
+    const $copyBtn = document.querySelector('.share-modal .opt-button')
+    const $shareInput = document.querySelector('.share-modal input')
+
+    renderPlain($previewPlain, $textarea.value)
+    renderMarkdown($previewMd, $textarea.value)
+
+    if ($textarea) {
+        $textarea.oninput = throttle(function () {
+            renderMarkdown($previewMd, $textarea.value)
+
+            $loading.style.display = 'inline-block'
+            const data = {
+                t: $textarea.value,
+            }
+
+            window.fetch('', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: new URLSearchParams(data),
+            })
+                .then(res => res.json())
+                .then(res => {
+                    if (res.err !== 0) {
+                        errHandle(res.msg)
+                    }
+                })
+                .catch(err => errHandle(err))
+                .finally(() => {
+                    $loading.style.display = 'none'
+                })
+        }, 1000)
+    }
+
+    if ($pwBtn) {
+        $pwBtn.onclick = function () {
+            const passwd = window.prompt('输入新密码（留空可以清除当前密码）')
+            if (passwd == null) return;
+
+            const path = window.location.pathname
+            window.fetch(`${path}/pw`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    passwd: passwd.trim(),
+                }),
+            })
+                .then(res => res.json())
+                .then(res => {
+                    if (res.err !== 0) {
+                        return errHandle(res.msg)
+                    }
+                    alert(`密码 ${passwd ? '设置' : '清除'} 成功!`)
+                })
+                .catch(err => errHandle(err))
+        }
+    }
+
+    if ($modeBtn) {
+        $modeBtn.onclick = function (e) {
+            const isMd = e.target.checked
+            const path = window.location.pathname
+            window.fetch(`${path}/setting`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    mode: isMd ? 'md' : 'plain',
+                }),
+            })
+                .then(res => res.json())
+                .then(res => {
+                    if (res.err !== 0) {
+                        return errHandle(res.msg)
+                    }
+
+                    window.location.reload()
+                })
+                .catch(err => errHandle(err))
+        }
+    }
+    
+    if ($shareBtn) {
+        $shareBtn.onclick = function (e) {
+            const isShare = e.target.checked
+            const path = window.location.pathname
+            window.fetch(`${path}/setting`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    share: isShare,
+                }),
+            })
+                .then(res => res.json())
+                .then(res => {
+                    if (res.err !== 0) {
+                        return errHandle(res.msg)
+                    }
+
+                    if (isShare) {
+                        const origin = window.location.origin
+                        const url = `${origin}/share/${res.data}`
+                        // show modal
+                        $shareInput.value = url
+                        $shareModal.style.display = 'block'
+                    }
+                })
+                .catch(err => errHandle(err))
+        }
+    }
+
+    if ($shareModal) {
+        $closeBtn.onclick = function () {
+            $shareModal.style.display = 'none'
+
+        }
+        $copyBtn.onclick = function () {
+            clipboardCopy($shareInput.value)
+            const originText = $copyBtn.innerHTML
+            const originColor = $copyBtn.style.background
+            $copyBtn.innerHTML = '复制成功!'
+            $copyBtn.style.background = 'orange'
+            window.setTimeout(() => {
+                $shareModal.style.display = 'none'
+                $copyBtn.innerHTML = originText
+                $copyBtn.style.background = originColor
+            }, 1500)
+        }
+    }
+
+})
